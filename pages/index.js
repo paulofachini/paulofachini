3b07b171a74e36bc92afcd97e8ca43fea527118c
Paulo Luiz Fachini <paulofachini@gmail.com>
2025-09-23 11:23:08 -0300

    homepage em construção.

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function HomePage() {
+  return (
+    <>
+      <h1>
+        <strong>🚧 Em Construção.</strong>
+      </h1>
+      <p>
+        👷🏼<a href="https://github.com/paulofachini">@paulofachini</a>
+      </p>
+    </>
+  );
+}
+
+export default HomePage;
